461c8bb59316ec001f30997d01d8cbee30d6f66d
Administrador <Administrador@LAB06-69>
2022-07-06 19:38:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("exercicio 4");
+            int i = 0;
+            while (i < 50)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
